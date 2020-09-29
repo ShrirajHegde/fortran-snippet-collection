26929dd21b30d7aab276b60ce4eb0e840fe98a8e
Shriraj <shriraj.hegde@gmail.com>
2020-09-29 20:37:35 +0530

    printMatrix subroutine added

---
+++ Functions & Subroutines/printMatrix -Subroutine to print 2d array like matrix.f90
@@ -0,0 +1,15 @@
+subroutine printMatrix(array_2d)
+        implicit none
+        integer::i,j,n,m,t(2)
+        real*8,intent(IN)::array_2d(:,:)
+        t=SHAPE(array_2d);n=t(1);m=t(2) !determining shape
+        do j=1,n
+            do i=1,m
+                write(*,"(F6.2)",advance='no') array_2d(j,i)  !add formatting as required
+                write(*,"(A2)",advance='no')" "
+            enddo
+            do i=1,2   !(1,k) where k is the number of empty rows inbetween rows
+                print*,''
+            enddo 
+        enddo
+    end subroutine printMatrix
