143232e93d08ba51eb3f610ea893f92690ac18b9
Shriraj <shriraj.hegde@gmail.com>
2020-11-10 18:26:40 +0530

    Rename files,add line count subroutine renames printmatrix subroutine

---
+++ Functions & Subroutines/lineCount.f90
@@ -0,0 +1,11 @@
+INTEGER function lineCount(filename1_of_50_char_len)
+    CHARACTER(len = 50)::filename1_of_50_char_len
+    lineCount = 0 
+    OPEN (1536, file = filename1_of_50_char_len,status='old')
+    DO
+        READ(1536,*,iostat=io)
+        IF (io.ne.0) EXIT
+        lineCount = lineCount + 1
+    END DO
+    CLOSE (1)
+end function lineCount
